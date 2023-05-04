889efba95a803df77b8d0e45eb1936928c23b7da
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2023-05-04 21:46:32 +0200

    Added endpoint to get a single books state

--- konyvtar/LibraryApplication/Controllers/BooksController.cs
+++ konyvtar/LibraryApplication/Controllers/BooksController.cs
@@ -20,5 +20,39 @@ namespace LibraryApplication.Controllers
             var books = await this._libraryContext.Books.ToListAsync();
             return this.Ok(books);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var book = await this._libraryContext.Books
+                .Where(b => b.InventoryNumber == id)
+                .Select(b => new
+                {
+                    Title = b.Title,
+                    Status = this._libraryContext.Borrows
+                        .Where(br => br.InventoryNumber == id)
+                        .Any() ? "borrowed" : "in",
+                    BorrowerName = this._libraryContext.Borrows
+                        .Where(br => br.InventoryNumber == id)
+                        .Join(
+                            this._libraryContext.Users,
+                            br => br.ReaderNumber,
+                            u => u.ReaderNumber,
+                            (br, u) => u.Name)
+                        .FirstOrDefault(),
+                    ReturnDate = this._libraryContext.Borrows
+                        .Where(br => br.InventoryNumber == id)
+                        .Select(br => br.ReturnDate)
+                        .FirstOrDefault(),
+                })
+                .FirstOrDefaultAsync();
+
+            if (book is null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(book);
+        }
     }
 }
