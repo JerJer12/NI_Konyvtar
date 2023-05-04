785d581f118b8ef8d503e004c11ffe095e1ff73e
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2023-05-04 21:41:49 +0200

    Created User Controller

---
+++ konyvtar/LibraryApplication/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly LibraryContext _libraryContext;
+
+        public UsersController(LibraryContext libraryContext)
+        {
+            this._libraryContext = libraryContext;
+        }
+    }
+}
