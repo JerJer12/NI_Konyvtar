bab797d637c194642cca04e1d76ff2878c8006f3
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2023-05-04 21:48:10 +0200

    Created Borrows Controller

---
+++ konyvtar/LibraryApplication/Controllers/BorrowsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace LibraryApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BorrowsController : Controller
+    {
+        private readonly LibraryContext _libraryContext;
+
+        public BorrowsController(LibraryContext libraryContext)
+        {
+            this._libraryContext = libraryContext;
+        }
+    }
+}
