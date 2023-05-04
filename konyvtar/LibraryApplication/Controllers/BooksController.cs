95b737e76776ed3de48ab09aba03dc1f9b08135f
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2023-05-04 21:45:23 +0200

    Created Books Controller

---
+++ konyvtar/LibraryApplication/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly LibraryContext _libraryContext;
+
+        public BooksController(LibraryContext libraryContext)
+        {
+            this._libraryContext = libraryContext;
+        }
+    }
+}
