bb3baf54459c25e84b74c92cfe0676516e8f86d1
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2023-05-04 21:48:42 +0200

    Added endpoint to get all the borrows

--- konyvtar/LibraryApplication/Controllers/BorrowsController.cs
+++ konyvtar/LibraryApplication/Controllers/BorrowsController.cs
@@ -14,5 +14,22 @@ namespace LibraryApplication.Controllers
         {
             this._libraryContext = libraryContext;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Borrow>>> Get()
+        {
+            var borrows = from borrow in this._libraryContext.Borrows
+                        join people in this._libraryContext.Users on borrow.ReaderNumber equals people.ReaderNumber
+                        join book in this._libraryContext.Books on borrow.InventoryNumber equals book.InventoryNumber
+                        select new
+                        {
+                            BorrowNumber = borrow.BorrowNumber,
+                            Name = people.Name,
+                            BookTitle = book.Title,
+                            BorrowDate = borrow.BorrowDate,
+                            ReturnDate = borrow.ReturnDate,
+                        };
+            return this.Ok(borrows);
+        }
     }
 }
