de250ea242146c4f5a036fcc18a4152b6a954f10
Teremi Bence <teremibence@gmail.com>
2023-05-15 22:15:03 +0200

    made unittest files

---
+++ konyvtar/LibrarianClient.Tests/AddUserUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using LibrarianClient.Service.LibraryService;
+using LibraryApplication.Contracts;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarianClient.Tests
+{
+    [TestClass]
+    internal class AddUserUnitTests
+    {
+
+        [TestMethod]
+       
+        public void AddNewUser()
+        {
+            User newUser = new User();
+
+            newUser.BirthDate = DateTime.Now;
+
+            
+        }  
+        
+
+    }
+}
