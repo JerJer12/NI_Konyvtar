26eaf3dfea21a943a20d4d727e4a323667bbf7f2
Teremi Bence <teremibence@gmail.com>
2023-05-16 00:01:12 +0200

    added unittests

--- konyvtar/LibrarianClient.Tests/AddUserUnitTests.cs
+++ konyvtar/LibrarianClient.Tests/AddUserUnitTests.cs
@@ -1,29 +1,187 @@
-﻿using LibrarianClient.Service.LibraryService;
+﻿
+using LibrarianClient.Pages;
+using LibrarianClient.Service.LibraryService;
 using LibraryApplication.Contracts;
-using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace LibrarianClient.Tests
 {
     [TestClass]
-    internal class AddUserUnitTests
+    public class AddUserUnitTests
     {
 
         [TestMethod]
-       
-        public void AddNewUser()
+
+        public void AddNewUser_WasBornBeforeToday()
         {
-            User newUser = new User();
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
 
-            newUser.BirthDate = DateTime.Now;
+           //libraryService.AddUser(newUser);
 
+            // Assert
+            Assert.IsTrue(newUser.BirthDate < DateTime.Today);
+                
+        }
+
+
+        [TestMethod]
+
+        public void AddNewUser_AddressAtleastTWoCharactersLong()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+            //libraryService.AddUser(newUser);
+
+            // Assert
+            Assert.IsTrue(newUser.Address.Length >=2);
+
+        }
+
+
+        [TestMethod]
+
+        public void AddNewUser_IsEmpty()
+        {
+            User newUser = new User
+            {
+                Name = "john",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+            AddUser(newUser);
+          
+
+            // Assert
+            Assert.IsFalse(string.IsNullOrEmpty(newUser.Name));
+
+        }
+
+        private void AddUser(User newUser)
+        {
             
-        }  
-        
+        }
+
+        [TestMethod]
+
+        public void AddNewUser_StartWithWhitePlace()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+
+            // Assert
+            Assert.IsFalse(newUser.Name.StartsWith(" "));
+
+        }
+
+        [TestMethod]
+
+        public void AddNewUser_EndsWithWhitePlace()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+
+            // Assert
+            Assert.IsFalse(newUser.Name.EndsWith(" "));
+
+        }
+
+        [TestMethod]
+
+        public void AddNewUser_IsNullOrWhiteSpace()
+        {
+            User newUser = new User
+            {
+                Name = "john",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+
+            // Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(newUser.Name));
+
+        }
+
+        [TestMethod]
+
+        public void AddNewUser_HasSpecialCharacters()
+        {
+            User newUser = new User
+            {
+                Name = "john!",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+            string pattern = @"[^a-zA-Z0-9]";
+
+            // Assert
+            Assert.IsFalse(Regex.IsMatch(newUser.Name, pattern));
+
+        }
+
+
+        /*
+        [TestMethod]
+
+        public void AddNewUser_WasBornToday()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today
+            };
+
+            //libraryService.AddUser(newUser);
+
+            // Assert
+            Assert.IsTrue(newUser.BirthDate < DateTime.Today);
+
+        }
+
+        [TestMethod]
+
+        public void AddNewUser_WasBornInTheFuture()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(1)
+            };
+
+            //libraryService.AddUser(newUser);
+
+            // Assert
+            Assert.IsTrue(newUser.BirthDate < DateTime.Today);
+
+        }
+        */
+
 
     }
+
+
 }
--- konyvtar/LibrarianClient.Tests/EditUserUnitTests.cs
+++ konyvtar/LibrarianClient.Tests/EditUserUnitTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using LibraryApplication.Contracts;
+using System.Text.RegularExpressions;
 
 namespace LibrarianClient.Tests
 {
@@ -10,7 +9,135 @@ namespace LibrarianClient.Tests
     internal class EditUserUnitTests
     {
 
+        [TestMethod]
+        public void SaveUser_WasBornBeforeToday()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main St",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
 
+            //libraryService.AddUser(newUser);
+
+            // Assert
+            Assert.IsTrue(newUser.BirthDate < DateTime.Today);
+
+        }
+
+
+        [TestMethod]
+
+        public void SavewUser_AddressAtleastTWoCharactersLong()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+            //libraryService.AddUser(newUser);
+
+            // Assert
+            Assert.IsTrue(newUser.Address.Length >= 2);
+
+        }
+
+
+        [TestMethod]
+
+        public void SaveUser_IsEmpty()
+        {
+            User newUser = new User
+            {
+                Name = "john",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+            SaveUser(newUser);
+
+
+            // Assert
+            Assert.IsFalse(string.IsNullOrEmpty(newUser.Name));
+
+        }
+
+        private void SaveUser(User newUser)
+        {
+
+        }
+
+        [TestMethod]
+
+        public void SaveUser_StartWithWhitePlace()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+
+            // Assert
+            Assert.IsFalse(newUser.Name.StartsWith(" "));
+
+        }
+
+        [TestMethod]
+
+        public void SaveUser_EndsWithWhitePlace()
+        {
+            User newUser = new User
+            {
+                Name = "John Doe",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+
+            // Assert
+            Assert.IsFalse(newUser.Name.EndsWith(" "));
+
+        }
+
+        [TestMethod]
+
+        public void SaveUser_IsNullOrWhiteSpace()
+        {
+            User newUser = new User
+            {
+                Name = "john",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+
+            // Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(newUser.Name));
+
+        }
+
+        [TestMethod]
+
+        public void SaveUser_HasSpecialCharacters()
+        {
+            User newUser = new User
+            {
+                Name = "john!",
+                Address = "Main Street",
+                BirthDate = DateTime.Today.AddDays(-1)
+            };
+
+            string pattern = @"[^a-zA-Z0-9]";
+
+            // Assert
+            Assert.IsFalse(Regex.IsMatch(newUser.Name, pattern));
+
+        }
 
     }
 }
